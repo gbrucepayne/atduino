c188de559d388e4617de17f6dd81d17fc795f897
gbrucepayne <gbrucepayne@hotmail.com>
2024-02-26 13:54:13 -0500

    namespace cleanup

--- unittests/test_desktop/test_atstringutils.cpp
+++ unittests/test_desktop/test_atstringutils.cpp
@@ -3,64 +3,64 @@
 
 void test_printableChar_ascii() {
   char c = 'A';
-  TEST_ASSERT_TRUE(atPrintableChar(c));
+  TEST_ASSERT_TRUE(at::printableChar(c));
 }
 
 void test_printableChar_null() {
   char c = 0;
-  TEST_ASSERT_FALSE(atPrintableChar(c));
+  TEST_ASSERT_FALSE(at::printableChar(c));
 }
 
 void test_printableChar_crlf() {
   char c = '\r';
-  TEST_ASSERT_TRUE(atPrintableChar(c));
+  TEST_ASSERT_TRUE(at::printableChar(c));
   c = '\n';
-  TEST_ASSERT_TRUE(atPrintableChar(c));
+  TEST_ASSERT_TRUE(at::printableChar(c));
 }
 
 void test_includes_str() {
   String test_str = "test string";
   String test_true = "test";
   String test_false = "strings";
-  TEST_ASSERT_TRUE(includes(test_str, test_true));
-  TEST_ASSERT_FALSE(includes(test_str, test_false));
+  TEST_ASSERT_TRUE(at::includes(test_str, test_true));
+  TEST_ASSERT_FALSE(at::includes(test_str, test_false));
 }
 
 void test_includes_cstr() {
   const char* test_str = "test string";
   const char* test_true = "test";
   const char* test_false = "strings";
-  TEST_ASSERT_TRUE(includes(test_str, test_true));
-  TEST_ASSERT_FALSE(includes(test_str, test_false));
+  TEST_ASSERT_TRUE(at::includes(test_str, test_true));
+  TEST_ASSERT_FALSE(at::includes(test_str, test_false));
 }
 
 void test_indexOf_cstr() {
   char test_str[] = "test string";
   const char pattern[] = "e";
   int expected = 1;
-  TEST_ASSERT_EQUAL(expected, indexOf(test_str, pattern));
+  TEST_ASSERT_EQUAL(expected, at::indexOf(test_str, pattern));
   const char pattern2[] = "E";
   expected = -1;
-  TEST_ASSERT_EQUAL(expected, indexOf(test_str, pattern2));
+  TEST_ASSERT_EQUAL(expected, at::indexOf(test_str, pattern2));
   char c = 'e';
   expected = 1;
-  TEST_ASSERT_EQUAL(expected, indexOf(test_str, c));
+  TEST_ASSERT_EQUAL(expected, at::indexOf(test_str, c));
 }
 
 void test_startsWith_cstr() {
   const char* test_str = "test string";
   const char* test_true = "test ";
   const char* test_false = "string";
-  TEST_ASSERT_TRUE(startsWith(test_str, test_true));
-  TEST_ASSERT_FALSE(startsWith(test_str, test_false));
+  TEST_ASSERT_TRUE(at::startsWith(test_str, test_true));
+  TEST_ASSERT_FALSE(at::startsWith(test_str, test_false));
 }
 
 void test_endsWith_cstr() {
   const char* test_str = "test string";
   const char* test_true = " string";
   const char* test_false = "t";
-  TEST_ASSERT_TRUE(endsWith(test_str, test_true));
-  TEST_ASSERT_FALSE(endsWith(test_str, test_false));
+  TEST_ASSERT_TRUE(at::endsWith(test_str, test_true));
+  TEST_ASSERT_FALSE(at::endsWith(test_str, test_false));
 }
 
 void test_substring_cstr() {
@@ -69,7 +69,7 @@ void test_substring_cstr() {
   uint8_t start = 0;
   uint8_t end = 4;
   char expected[] = "test";
-  substring(target, original, start, end);
+  at::substring(target, original, start, end);
   #if defined TEST_ASSERT_EQUAL_CHAR_ARRAY
   TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, target, strlen(expected));
   #endif
@@ -80,7 +80,7 @@ void test_substring_cstr_to_end() {
   char original[] = "test string";
   uint8_t start = 5;
   char expected[] = "string";
-  substring(target, original, start);
+  at::substring(target, original, start);
   #if defined TEST_ASSERT_EQUAL_CHAR_ARRAY
   TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, target, strlen(expected));
   #endif
@@ -96,18 +96,18 @@ void test_replace_cstr() {
   char to_replace[] = "\r\nOK\r\n";
   char replace_with[] = "";
   char expected[] = "\r\n+GSN: 00000000SKYEE3D\r\n";
-  replace(cstr, to_replace, replace_with, buffersize);
+  at::replace(cstr, to_replace, replace_with, buffersize);
   #if defined TEST_ASSERT_EQUAL_CHAR_ARRAY
   TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, cstr, strlen(expected));
   #else
-  LOG_INFO("\r\n  Expected:", atDebugString(expected), "\r\n  Got:", atDebugString(cstr));
+  LOG_INFO("\r\n  Expected:", at::debugString(expected), "\r\n  Got:", at::debugString(cstr));
   #endif
 }
 
 void test_remove_cstr() {
   char test_cstr[] = "test string";
   char expected[] = "test";
-  remove(test_cstr, 4);
+  at::remove(test_cstr, 4);
   #if defined TEST_ASSERT_EQUAL_CHAR_ARRAY
   TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, test_cstr, 4);
   #endif
@@ -116,7 +116,7 @@ void test_remove_cstr() {
 void test_trim_cstr() {
   char test_cstr[32] = "\r\ntest string \r\n";
   char expected[] = "test string";
-  trim(test_cstr, 32);
+  at::trim(test_cstr, 32);
   #if defined TEST_ASSERT_EQUAL_CHAR_ARRAY
   TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, test_cstr, 4);
   #endif
@@ -125,7 +125,7 @@ void test_trim_cstr() {
 void test_intToHex_cstr() {
   int val = 255;
   char result[4+1];
-  intToHex(result, val, 4, 4+1);
+  at::intToHex(result, val, 4, 4+1);
   char expected[] = "00FF";
   #if defined TEST_ASSERT_EQUAL_CHAR_ARRAY
   TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, result, 4);
@@ -135,18 +135,18 @@ void test_intToHex_cstr() {
 void test_hexToInt_cstr() {
   char test_cstr[] = "00FF";
   int expected = 255;
-  TEST_ASSERT_EQUAL(expected, hexToInt(test_cstr));
+  TEST_ASSERT_EQUAL(expected, at::hexToInt(test_cstr));
   char test_cstr2[] = "86C5";
   int expected2 = 34501;
-  TEST_ASSERT_EQUAL(expected2, hexToInt(test_cstr2));
+  TEST_ASSERT_EQUAL(expected2, at::hexToInt(test_cstr2));
 }
 
 void test_base64Encode() {
   unsigned char data[] = { 1, 2, 3, 4 };
   size_t bufferlen = sizeof(data) / sizeof(data[0]);
-  size_t b64_len = base64StringLength(bufferlen) + 1;
+  size_t b64_len = at::base64StringLength(bufferlen) + 1;
   char result[b64_len];
-  base64Encode(result, data, bufferlen);
+  at::base64Encode(result, data, bufferlen);
   char expected[] = "AQIDBA==";
   #if defined TEST_ASSERT_EQUAL_CHAR_ARRAY
   TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, result, strlen(expected));
@@ -155,13 +155,13 @@ void test_base64Encode() {
 
 void test_base64BufferLength() {
   char b64_cstr[] = "AQIDBA==";
-  TEST_ASSERT_EQUAL(4, base64BufferLength(b64_cstr));
+  TEST_ASSERT_EQUAL(4, at::base64BufferLength(b64_cstr));
 }
 
 void test_base64Decode() {
   char b64_cstr[] = "AQIDBA==";
   char result[4];
-  base64Decode(result, b64_cstr);
+  at::base64Decode(result, b64_cstr);
   char expected[] = { 1, 2, 3, 4 };
   #if defined TEST_ASSERT_EQUAL_CHAR_ARRAY
   TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, result, 4);
--- unittests/test_desktop/test_crcxmodem.cpp
+++ unittests/test_desktop/test_crcxmodem.cpp
@@ -3,7 +3,7 @@
 
 void test_applyCrc_cstr() {
   char cstr[32] = "AT%CRC=0";
-  applyCrc(cstr, 32);
+  at::applyCrc(cstr, 32);
   char expected[] = "AT%CRC=0*BBEB";
   #if defined TEST_ASSERT_EQUAL_CHAR_ARRAY
   TEST_ASSERT_EQUAL_CHAR_ARRAY(expected, cstr, strlen(expected));
@@ -12,5 +12,5 @@ void test_applyCrc_cstr() {
 
 void test_validateCrc_cstr() {
   char at_response[] = "\r\nOK\r\n*86C5\r\n";
-  TEST_ASSERT_TRUE(validateCrc(at_response));
+  TEST_ASSERT_TRUE(at::validateCrc(at_response));
 }
